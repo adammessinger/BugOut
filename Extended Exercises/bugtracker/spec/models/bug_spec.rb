0088840a0786dd5934e2df393547df62eac5f20e
adammessinger <adam@adammessinger.com>
2015-07-02 13:00:28 -0500

    Add Test for Bug Assignee Record Validation

--- Extended Exercises/bugtracker/spec/models/bug_spec.rb
+++ Extended Exercises/bugtracker/spec/models/bug_spec.rb
@@ -7,10 +7,17 @@ describe Bug do
   it { should validate_presence_of(:reporter_id) }
   it { should validate_presence_of(:description) }
 
-  it 'validates associated reporter' do
+  it 'validates associated reporter record' do
     bug = Bug.new(title: 'foo', description: 'bar')
     bug.build_reporter(id: 9999, email: nil)
     expect(bug.save).to(eq false)
     expect(bug.errors.full_messages_for(:reporter)[0]).to(eq 'Reporter is invalid')
+  end
+
+  it 'validates associated assignee record' do
+    bug = Bug.new(title: 'foo', description: 'bar')
+    bug.build_assignee(id: 9999, email: nil)
+    expect(bug.save).to(eq false)
+    expect(bug.errors.full_messages_for(:assignee)[0]).to(eq 'Assignee is invalid')
   end
 end
