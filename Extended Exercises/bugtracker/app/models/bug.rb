3e1665262547f1394e5b8b42c7609aea84a45ea3
adammessinger <adam@adammessinger.com>
2015-07-02 16:15:24 -0500

    Fix Bad Association in Bug Model
    This was causing a variety of problems on show and edit.

--- Extended Exercises/bugtracker/app/models/bug.rb
+++ Extended Exercises/bugtracker/app/models/bug.rb
@@ -1,6 +1,6 @@
 class Bug < ActiveRecord::Base
-  belongs_to :reporter, class_name: "User", foreign_key: 'id', validate: true
-  belongs_to :assignee, class_name: "User", foreign_key: 'id', validate: true
+  belongs_to :reporter, class_name: "User", foreign_key: 'reporter_id', validate: true
+  belongs_to :assignee, class_name: "User", foreign_key: 'assignee_id', validate: true
 
   validates(:title, presence: true)
   validates(:reporter_id, presence: true)
