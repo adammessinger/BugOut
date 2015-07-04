121e230bd67cc68c11b7c3890dcb6709caba13bd
adammessinger <adam@adammessinger.com>
2015-07-04 13:58:08 -0500

    Make Bug Comments Usable
    Edits to generated scaffolding for comments to make them actually work.

--- Extended Exercises/bugtracker/app/controllers/comments_controller.rb
+++ Extended Exercises/bugtracker/app/controllers/comments_controller.rb
@@ -1,34 +1,27 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :set_bug
 
-  # GET /comments
-  # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = @bug.comments
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
+  # def show
+  # end
 
-  # GET /comments/new
   def new
-    @comment = Comment.new
+    @comment = Comment.new(author_id: current_user.id, bug_id: @bug.id)
   end
 
-  # GET /comments/1/edit
   def edit
+    @comment = @bug.comments.find(params[:id])
   end
 
-  # POST /comments
-  # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @bug.comments.create(comment_params)
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @bug, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -37,12 +30,12 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
+    @comment = @bug.comments.find(params[:id])
+
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @bug, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -51,20 +44,20 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
+    @comment = @bug.comments.find(params[:id])
+
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to @bug, notice: 'Comment was successfully deleted.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
+    def set_bug
+      @bug = Bug.find(params[:bug_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- Extended Exercises/bugtracker/app/views/comments/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @comment, :id, :author_id, :bug_id, :body, :created_at, :updated_at
