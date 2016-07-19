b783c86b764b05828dabde0551b59af56ddba8a6
adammessinger <adammessinger@users.noreply.github.com>
2016-07-18 20:56:49 -0500

    Fix Bug Index "With Bugs" Cases
    Fixed "with 1 bug" context
    Fixed "with 2 bugs" context
    Fixed "with open and closed bugs" context
    DRYed out bug test case creation

--- spec/views/bugs/index.html.erb_spec.rb
+++ spec/views/bugs/index.html.erb_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
+# TODO: tests for tags, assignment (with / without)
+
 RSpec.describe 'bugs/index', type: :view do
+  # This should return the minimal set of attributes required to create a valid
+  # Bug. Remember to keep this up to date (or better, switch to fixtures or FactoryGirl).
+  let(:valid_attributes) {
+    {
+      title: 'My Title',
+      description: 'My Text',
+      closed: false,
+      reporter_id: 1
+    }
+  }
+
   context 'with 0 bugs' do
     before(:each) do
       # NOTE: see https://github.com/rspec/rspec-rails/issues/396
@@ -33,13 +46,7 @@ RSpec.describe 'bugs/index', type: :view do
 
   context 'with 1 bug' do
     before(:each) do
-      assign(:bugs, [
-        Bug.create!(
-          title: 'My Title',
-          description: 'My Text',
-          closed: false
-        )
-      ])
+      assign(:bugs, [Bug.create!(valid_attributes)])
     end
 
     it 'has a "1 Bug" heading' do
@@ -50,18 +57,9 @@ RSpec.describe 'bugs/index', type: :view do
 
   context 'with 2 bugs' do
     before(:each) do
-      assign(:bugs, [
-        Bug.create!(
-          title: 'My Title',
-          description: 'My Description',
-          closed: false
-        ),
-        Bug.create!(
-          title: 'My Title',
-          description: 'My Description',
-          closed: false
-        )
-      ])
+      bugs = []
+      2.times { bugs << Bug.create!(valid_attributes) }
+      assign(:bugs, bugs)
     end
 
     it 'has a "2 Bugs" heading' do
@@ -73,54 +71,52 @@ RSpec.describe 'bugs/index', type: :view do
       render
       assert_select 'thead th:nth-child(1)', { text: 'ID', count: 1 }
       assert_select 'thead th:nth-child(2)', { text: 'Title', count: 1 }
-      assert_select 'thead th:nth-child(3)', { text: 'Closed?', count: 1 }
-      assert_select 'thead th:nth-child(4)', { text: 'Opened', count: 1 }
-      assert_select 'thead th:nth-child(5)', { text: 'Updated', count: 1 }
-      assert_select 'thead th:nth-child(6)', { text: 'Actions', count: 1 }
+      assert_select 'thead th:nth-child(3)', { text: 'Assignee', count: 1 }
+      assert_select 'thead th:nth-child(4)', { text: 'Tags', count: 1 }
+      assert_select 'thead th:nth-child(5)', { text: 'Closed?', count: 1 }
+      assert_select 'thead th:nth-child(6)', { text: 'Opened', count: 1 }
+      assert_select 'thead th:nth-child(7)', { text: 'Updated', count: 1 }
+      assert_select 'thead th:nth-child(8)', { text: 'Actions', count: 1 }
     end
 
     it 'renders a list of all bugs' do
       render
       assert_select 'tr > td:nth-child(1)', { text: /^\d+$/, count: 2 }
-      assert_select 'tr > td:nth-child(2)', { text: 'My Title'.to_s, count: 2 }
-      assert_select 'tr > td:nth-child(3)', { text: false.to_s, count: 2 }
-      assert_select 'tr > td:nth-child(4)', { text: Time.now.strftime('%Y-%m-%d'), count: 2 }
-      assert_select 'tr > td:nth-child(5)', { text: Time.now.strftime('%Y-%m-%d'), count: 2 }
-      assert_select 'tr > td:nth-child(6) > a[href^="/bugs/"][href$="/edit"]', { text: 'Edit', count: 2 }
-      assert_select 'tr > td:nth-child(6) > a[data-method="delete"]', { text: 'Delete', count: 2 }
+      assert_select 'tr > td:nth-child(2)', { text: valid_attributes[:title], count: 2 }
+      assert_select 'tr > td:nth-child(3)', { text: 'unassigned', count: 2 }
+      assert_select 'tr > td:nth-child(4)', { text: '', count: 2 }
+      assert_select 'tr > td:nth-child(5)', { text: false.to_s, count: 2 }
+      assert_select 'tr > td:nth-child(6)', { text: Time.now.strftime('%Y-%m-%d'), count: 2 }
+      assert_select 'tr > td:nth-child(7)', { text: Time.now.strftime('%Y-%m-%d'), count: 2 }
+      assert_select 'tr > td:nth-child(8) > a[href^="/bugs/"][href$="/edit"]', { text: 'Edit', count: 2 }
+      assert_select 'tr > td:nth-child(9) > a[data-method="delete"]', { text: 'Delete', count: 2 }
     end
   end
 
   context 'with closed and unclosed bugs' do
     before(:each) do
-      assign(:bugs, [
-        Bug.create!(
-          title: 'My Title',
-          description: 'My Description',
-          closed: true
-        ),
-        Bug.create!(
-          title: 'My Title',
-          description: 'My Description',
-          closed: false
-        )
-      ])
+      bugs = []
+      [true, false].each do |b|
+        valid_attributes[:closed] = b
+        bugs << Bug.create!(valid_attributes)
+      end
+      assign(:bugs, bugs)
     end
 
     it 'wraps closed bug titles in <s> tag' do
       render
-      assert_select 'tr > td:nth-child(2)', { text: 'My Title'.to_s, count: 2 }
-      assert_select 'tr > td:nth-child(2) > s', { text: 'My Title'.to_s, count: 1 }
+      assert_select 'tr > td:nth-child(2)', { text: valid_attributes[:title], count: 2 }
+      assert_select 'tr > td:nth-child(2) s', { text: valid_attributes[:title], count: 1 }
     end
 
     it 'gives the "bg-success" class to "Closed?" cell for closed bugs' do
       render
-      assert_select 'tr > td:nth-child(3).bg-success', { text: true.to_s, count: 1 }
+      assert_select 'tr > td:nth-child(5).bg-success', { text: true.to_s, count: 1 }
     end
 
     it 'gives the "bg-danger" class to "Closed?" cell for unclosed bugs' do
       render
-      assert_select 'tr > td:nth-child(3).bg-danger', { text: false.to_s, count: 1 }
+      assert_select 'tr > td:nth-child(5).bg-danger', { text: false.to_s, count: 1 }
     end
   end
 end
