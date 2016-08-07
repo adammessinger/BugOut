c7b44e88fb1bdc4cc878eddf524d1e2e64e75e43
adammessinger <adammessinger@users.noreply.github.com>
2016-08-06 20:26:22 -0500

    Note Reason for Skipping Request Specs

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,3 +1,6 @@
+# NOTE: Saving request specs for testing JSON API whenever that's ready.
+# Meanwhile, specs for controllers, models, some views + Capybara integration tests.
+
 # require 'rails_helper'
 #
 # RSpec.describe "Comments", type: :request do
