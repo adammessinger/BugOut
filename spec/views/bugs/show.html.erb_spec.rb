ddea0d3c5c1f71824f2220cf4d9f513f0334ed08
adammessinger <adammessinger@users.noreply.github.com>
2016-08-07 23:35:32 -0500

    Add bugs/show View Spec
    Consider using "assert_select" instead of 
    "expect".

---
+++ spec/views/bugs/show.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'bugs/show', type: :view do
+  before(:each) do
+    complete_bug = Bug.new({
+      title: 'Title',
+      description: 'MyText',
+      closed: false,
+      tags: 'foo, bar, baz'
+    })
+    complete_bug.create_assignee email: 'jon@example.com', password: '29funaoiw3fuq345!@'
+    complete_bug.create_reporter email: 'jane@example.com', password: '29funaoiw3fuq345!@'
+    complete_bug.save
+    assign :bug, complete_bug
+    render
+  end
+
+  it 'renders bug attributes in a <dl>' do
+    expect(rendered).to(match(/Title/))
+    expect(rendered).to(match(/MyText/))
+    expect(rendered).to(match(/false/))
+    expect(rendered).to(match(/jon@example.com/))
+    expect(rendered).to(match(/jane@example.com/))
+    expect(rendered).to(match(/foo, bar, baz/))
+  end
+end
