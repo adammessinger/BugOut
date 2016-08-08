2680d9c7390ca44d9dd98093aa0fc8b0409a57b3
adammessinger <adammessinger@users.noreply.github.com>
2016-08-07 23:32:22 -0500

    Add Tags Field to bugs/new View Spec

--- spec/views/bugs/new.html.erb_spec.rb
+++ spec/views/bugs/new.html.erb_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe 'bugs/new', type: :view do
       assert_select 'select#bug_reporter_id[name=?]', 'bug[reporter_id]'
       assert_select 'select#bug_assignee_id[name=?]', 'bug[assignee_id]'
       assert_select 'textarea#bug_description[name=?]', 'bug[description]'
+      assert_select 'input#bug_tags[name=?]', 'bug[tags]'
       assert_select 'input#bug_closed[name=?]', 'bug[closed]'
       assert_select 'input[type=submit].btn-primary[name=?]', 'commit'
     end
