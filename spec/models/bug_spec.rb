759adec5a53aae92f795a66cf84bfca749331a24
adammessinger <adammessinger@users.noreply.github.com>
2016-08-26 21:58:38 -0500

    Add Tests for Bug#owned_by?
    Will check to see if passed User argument is the reporter 
    or assignee of a Bug instance.

--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -14,65 +14,107 @@ describe Bug, type: :model do
   it { should validate_presence_of(:reporter_id) }
   it { should validate_presence_of(:description) }
 
-  context 'with required fields filled' do
-    it 'is valid with a title, description, and reporter_id' do
-      bug = Bug.new(valid_attributes)
-      expect(bug).to(be_valid)
+  describe 'validation checking' do
+    context 'with required fields filled' do
+      it 'is valid with a title, description, and reporter_id' do
+        bug = Bug.new(valid_attributes)
+        expect(bug).to(be_valid)
+      end
+    end
+
+    context 'with required fields not filled' do
+      it 'is invalid without a title' do
+        valid_attributes[:title] = nil
+        bug = Bug.new(valid_attributes)
+        bug.valid?
+        expect(bug.errors[:title][0]).to(eq "can't be blank")
+      end
+
+      it 'is invalid without a reporter_id' do
+        valid_attributes[:reporter_id] = nil
+        bug = Bug.new(valid_attributes)
+        bug.valid?
+        expect(bug.errors[:reporter_id][0]).to(eq "can't be blank")
+      end
+
+      it 'is invalid without a description' do
+        valid_attributes[:description] = nil
+        bug = Bug.new(valid_attributes)
+        bug.valid?
+        expect(bug.errors[:description][0]).to(eq "can't be blank")
+      end
+    end
+
+    context 'with valid or invalid User associations' do
+      before(:example) do
+        valid_attributes[:reporter_id] = nil
+        @bug = Bug.new(valid_attributes)
+      end
+
+      it 'is valid if reporter record is valid' do
+        @bug.create_reporter!(user_jane)
+        @bug.valid?
+        expect(@bug).to(be_valid)
+      end
+
+      it 'is valid if assignee record is valid' do
+        @bug.create_reporter!(user_bob)
+        @bug.create_assignee!(user_jane)
+        @bug.valid?
+        expect(@bug).to(be_valid)
+      end
+
+      it 'is invalid if reporter record is invalid' do
+        @bug.build_reporter(id: 9999, email: nil)
+        @bug.save
+        expect(@bug.errors[:reporter][0]).to(eq 'is invalid')
+      end
+
+      it 'is invalid if assignee record is invalid' do
+        @bug.build_assignee(id: 9999, email: nil)
+        @bug.save
+        expect(@bug.errors[:assignee][0]).to(eq 'is invalid')
+      end
     end
   end
 
-  context 'with required fields not filled' do
-    it 'is invalid without a title' do
-      valid_attributes[:title] = nil
-      bug = Bug.new(valid_attributes)
-      bug.valid?
-      expect(bug.errors[:title][0]).to(eq "can't be blank")
-    end
-
-    it 'is invalid without a reporter_id' do
-      valid_attributes[:reporter_id] = nil
-      bug = Bug.new(valid_attributes)
-      bug.valid?
-      expect(bug.errors[:reporter_id][0]).to(eq "can't be blank")
-    end
-
-    it 'is invalid without a description' do
-      valid_attributes[:description] = nil
-      bug = Bug.new(valid_attributes)
-      bug.valid?
-      expect(bug.errors[:description][0]).to(eq "can't be blank")
-    end
-  end
-
-  context 'with valid or invalid User associations' do
+  describe '#owned_by?' do
     before(:example) do
-      valid_attributes[:reporter_id] = nil
       @bug = Bug.new(valid_attributes)
+      @bob = User.new(user_bob)
+      @jane = User.new(user_jane)
+      @bug.reporter = @bob
+      @bug.assignee = @jane
     end
 
-    it 'is valid if reporter record is valid' do
-      @bug.create_reporter!(user_jane)
-      @bug.valid?
-      expect(@bug).to(be_valid)
+    it 'returns true if passed user arg matches bug reporter' do
+      expect(@bug.owned_by?(@bob)).to(be true)
     end
 
-    it 'is valid if assignee record is valid' do
-      @bug.create_reporter!(user_bob)
-      @bug.create_assignee!(user_jane)
-      @bug.valid?
-      expect(@bug).to(be_valid)
+    it 'returns true if passed user arg matches bug assignee' do
+      expect(@bug.owned_by?(@jane)).to(be true)
     end
 
-    it 'is invalid if reporter record is invalid' do
-      @bug.build_reporter(id: 9999, email: nil)
-      @bug.save
-      expect(@bug.errors[:reporter][0]).to(eq 'is invalid')
+    it 'returns true if passed user arg matches bug reporter and assignee' do
+      @bug.reporter = @jane
+
+      expect(@bug.owned_by?(@jane)).to(be true)
     end
 
-    it 'is invalid if assignee record is invalid' do
-      @bug.build_assignee(id: 9999, email: nil)
-      @bug.save
-      expect(@bug.errors[:assignee][0]).to(eq 'is invalid')
+    it 'returns false if passed user arg matches NIETHER reporter NOR assignee' do
+      alice = User.new(email: 'alice@example.com', password: 'apwrhp9^%/*&z#x!vjdf')
+
+      expect(@bug.owned_by?(alice)).to(be false)
+    end
+
+    it 'returns nil if passed a non-user argument' do
+      nonsense = Object.new
+
+      expect(@bug.owned_by?(nonsense)).to(be nil)
+    end
+
+    it 'returns nil if passed no argument (user param defaults to nil)' do
+      expect(@bug.owned_by?).to(be nil)
     end
   end
 end
