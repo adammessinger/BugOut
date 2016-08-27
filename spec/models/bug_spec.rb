dc83c881e5293f5858588b4cb1a51e5ba5b7f2fa
adammessinger <adammessinger@users.noreply.github.com>
2016-08-27 00:24:01 -0500

    Use Better Names for Bug Owner Attributes Hash

--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -4,8 +4,8 @@ describe Bug, type: :model do
   let(:valid_attributes) do
     { title: 'My Title', description: 'My Text', closed: false, reporter_id: 1 }
   end
-  let(:user_bob) { { email: 'bob@example.com', password: '234&098qtpg9a732' } }
-  let(:user_jane) { { email: 'jane@example.com', password: '=45y8hj@pob#n8e4' } }
+  let(:user_bob_atts) { { email: 'bob@example.com', password: '234&098qtpg9a732' } }
+  let(:user_jane_atts) { { email: 'jane@example.com', password: '=45y8hj@pob#n8e4' } }
 
   it { should belong_to(:reporter) }
   it { should belong_to(:assignee) }
@@ -56,14 +56,14 @@ describe Bug, type: :model do
       end
 
       it 'is valid if reporter record is valid' do
-        @bug.create_reporter!(user_jane)
+        @bug.create_reporter!(user_jane_atts)
         @bug.valid?
         expect(@bug).to(be_valid)
       end
 
       it 'is valid if assignee record is valid' do
-        @bug.create_reporter!(user_bob)
-        @bug.create_assignee!(user_jane)
+        @bug.create_reporter!(user_bob_atts)
+        @bug.create_assignee!(user_jane_atts)
         @bug.valid?
         expect(@bug).to(be_valid)
       end
@@ -85,8 +85,8 @@ describe Bug, type: :model do
   describe '#owned_by?' do
     before(:example) do
       @bug = Bug.new(valid_attributes)
-      @bob = User.new(user_bob)
-      @jane = User.new(user_jane)
+      @bob = User.new(user_bob_atts)
+      @jane = User.new(user_jane_atts)
       @bug.reporter = @bob
       @bug.assignee = @jane
     end
