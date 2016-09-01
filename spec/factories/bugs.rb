49d2c80e4e8ef559553a2bda7bafcd3048546333
adammessinger <adammessinger@users.noreply.github.com>
2016-09-01 16:35:05 -0500

    Fix Problems with Escaped Apostrophe in Bug View Tests
    Replaced ' with ’ to avoid problems with escaping-caused 
    test failures.

--- spec/factories/bugs.rb
+++ spec/factories/bugs.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory(:bug, class: 'Bug') do
-    title { "#{Faker::Commerce.product_name} can't #{Faker::Company.bs}" }
+    title { "#{Faker::Commerce.product_name} can’t #{Faker::Company.bs}" }
     reporter
     description { "#{Faker::Lorem.paragraph}\n#{Faker::Hacker.say_something_smart}" }
     closed { [true, false].sample }
