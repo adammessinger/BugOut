20a7eefe9667fa854a6f6889271c35bad41a9fa2
adammessinger <adammessinger@users.noreply.github.com>
2016-09-01 20:33:20 -0500

    Expand Bug Factory to Include a Factory for Bugs with Comments

--- spec/factories/bugs.rb
+++ spec/factories/bugs.rb
@@ -3,5 +3,15 @@ FactoryGirl.define do
     title { "#{Faker::Commerce.product_name} can’t #{Faker::Company.bs}" }
     reporter
     description { "#{Faker::Lorem.paragraph}\n#{Faker::Hacker.say_something_smart}" }
+
+    factory(:bug_with_comments) do
+      transient do
+        comments_count(3)
+      end
+
+      after(:create) do |bug, evaluator|
+        create_list(:comment, evaluator.comments_count, bug: bug)
+      end
+    end
   end
 end
