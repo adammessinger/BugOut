a9b9025c0d85d02351bca7e078077f3320ddfadd
adammessinger <adammessinger@users.noreply.github.com>
2016-09-27 01:30:53 -0500

    Add Tests to Check for New Validations on User Model
    Will add further tests to confirm the validations work 
    later.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
-describe User do
+describe User, type: :model do
   it { should have_many(:reports) }
   it { should have_many(:assignments) }
   it { should have_many(:comments) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(2).is_at_most(255) }
+
+  describe 'validates case-insensitive uniqueness of email address' do
+    # NOTE: Must build a User instance that satisfies all schema constraints due to the way validate_uniqueness_of works.
+    # Details: https://github.com/thoughtbot/shoulda-matchers/blob/master/lib/shoulda/matchers/active_record/validate_uniqueness_of_matcher.rb#L26
+    subject { build(:user) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+  end
 end
